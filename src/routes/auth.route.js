013576f29c23e351b6f7c3c69f59819ec2264773
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2026-03-10 10:45:09 +0500

    fixed problems

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { register, login, me } from "../controllers/auth.js";
-import { authMiddleware } from "../middleware/auth.js";
+import { register, login, me } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 const router = express.Router();
 
