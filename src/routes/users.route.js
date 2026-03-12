6ff707e8feb6c351f4da8522be005ae839fd76e7
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2026-03-12 15:42:46 +0500

    refactor user routes by adding param and body validation

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,21 +1,41 @@
 import express from "express";
+
 import {
   getUsers,
   getUser,
   updateUser,
   deleteUser,
 } from "../controllers/users.controller.js";
+
 import { authMiddleware } from "../middlewares/auth.js";
 import { adminMiddleware } from "../middlewares/role.js";
+import { validate } from "../middlewares/validate.js";
+
+import {
+  updateUserSchema,
+  idParamSchema,
+} from "../validators/users.validator.js";
 
 const router = express.Router();
 
-// Hammasi adminga maxsus
+// Admin-only routes
 router.use(authMiddleware, adminMiddleware);
 
+// GET all users
 router.get("/", getUsers);
-router.get("/:id", getUser);
-router.patch("/:id", updateUser);
-router.delete("/:id", deleteUser);
+
+// GET single user
+router.get("/:id", validate(idParamSchema, "params"), getUser);
+
+// UPDATE user
+router.patch(
+  "/:id",
+  validate(idParamSchema, "params"),
+  validate(updateUserSchema),
+  updateUser,
+);
+
+// DELETE user
+router.delete("/:id", validate(idParamSchema, "params"), deleteUser);
 
 export default router;
