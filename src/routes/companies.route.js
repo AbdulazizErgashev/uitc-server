d10398eff2b9c6515bb8e839bf6a2a90adeef0b5
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2026-03-12 16:08:54 +0500

    refactor(companies): add zod validation for company routes

--- src/routes/companies.route.js
+++ src/routes/companies.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 import {
   getCompanies,
   getCompany,
@@ -6,17 +7,40 @@ import {
   updateCompany,
   deleteCompany,
 } from "../controllers/companies.controller.js";
+
 import { authMiddleware } from "../middlewares/auth.js";
 import { adminMiddleware } from "../middlewares/role.js";
+import { validate } from "../middlewares/validate.js";
+
+import {
+  createCompanySchema,
+  updateCompanySchema,
+  idParamSchema,
+} from "../validators/companies.validator.js";
 
 const router = express.Router();
 
+// Admin-only routes
 router.use(authMiddleware, adminMiddleware);
 
+// GET all companies
 router.get("/", getCompanies);
-router.get("/:id", getCompany);
-router.post("/", createCompany);
-router.patch("/:id", updateCompany);
-router.delete("/:id", deleteCompany);
+
+// GET single company
+router.get("/:id", validate(idParamSchema, "params"), getCompany);
+
+// CREATE company
+router.post("/", validate(createCompanySchema), createCompany);
+
+// UPDATE company
+router.patch(
+  "/:id",
+  validate(idParamSchema, "params"),
+  validate(updateCompanySchema),
+  updateCompany,
+);
+
+// DELETE company
+router.delete("/:id", validate(idParamSchema, "params"), deleteCompany);
 
 export default router;
